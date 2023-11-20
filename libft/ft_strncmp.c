3b625063e7a56128a1da90378a5213dc30a0784b
tesla33io <artemstavropolov@gmail.com>
2023-11-20 18:29:56 +0100

    fix: `strncmp` according to `francinette`.

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -12,12 +12,35 @@
 
 #include "libft.h"
 
+/**
+ * Compares 'n' bytes of two strings.
+ *
+ * @param s1 Pointer to the first string.
+ * @param s2 Pointer to the second string.
+ * @param n Number of bytes to compare.
+ * @return An integer representing the comparison result:
+ * - 0 > if the first differing byte in s1 is less than that in s2.
+ * - 0 < if the first differing byte in s1 is greater than that in s2.
+ * - 0 = if the first 'n' bytes of s1 and s2 are equal.
+ *
+ * Compares 'n' bytes of the strings pointed to by 's1' and 's2'. If 'n' reaches
+ * zero or a mismatch is found, the function returns the difference between the
+ * respective byte values.
+ */
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 && *s2 && *s1 == *s2 && --n)
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	str1 = (const unsigned char *) s1;
+	str2 = (const unsigned char *) s2;
+	if (n == 0)
+		return (0);
+	while (*str1 && *str2 && *str1 == *str2 && --n)
 	{
-		s1++;
-		s2++;
+		str1++;
+		str2++;
 	}
-	return (*s1 - *s2);
+	return (*str1 - *str2);
 }
